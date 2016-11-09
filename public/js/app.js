09be9ea5355c4f28c4e082cd041c4ff963e83026
MrDrannoc <poyer.k@gmail.com>
2016-11-09 20:58:51 +0100

    ajout service et controller moustache

--- public/js/app.js
+++ public/js/app.js
@@ -1,6 +1,8 @@
 angular.module('app', ['ngRoute'])
         .service('todoService', todoService)
+        .service('moustacheService', moustacheService)
         .controller('mainController', mainController)
+        .controller('moustacheController', moustacheController)
         .controller('navbarController', navbarController)
         .config(routes)
         ;
