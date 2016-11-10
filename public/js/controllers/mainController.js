6e4a6ea1346847b36a55a30d367f8020aa891ba0
MrDrannoc <poyer.k@gmail.com>
2016-11-10 08:47:52 +0100

    bouton collapse

--- public/js/controllers/mainController.js
+++ public/js/controllers/mainController.js
@@ -3,31 +3,40 @@ class mainController {
     constructor(todoService) {
         this.todoService = todoService;
         this.load();
+
+        $('.button-collapse').sideNav({
+        menuWidth: 300, // Default is 240
+        edge: 'left', // Choose the horizontal origin
+        closeOnClick: true, // Closes side-nav on <a> clicks, useful for Angular/Meteor
+        draggable: true // Choose whether you can drag to open on touch screens
+      }
+    );
+
     }
 
     load() {
         this.todoService.getAll().then((res) => {
             this.todos = res.data;
-        })
+        });
     }
 
     create() {
         this.todoService.create(this.todo).then(() => {
             this.todo = '';
-            this.load()
-        })
+            this.load();
+        });
     }
 
     update(todo) {
         this.todoService.update(todo._id, todo.description).then(() => {
-            this.load()
-        })
+            this.load();
+        });
     }
 
     delete(todo) {
         this.todoService.delete(todo._id).then(() => {
-            this.load()
-        })
+            this.load();
+        });
     }
 
 }
